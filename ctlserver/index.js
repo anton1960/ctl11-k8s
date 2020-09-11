9f62b638bb28ce0f2da17a3814894b407926c44d
anton <anton@localhost.localdomain>
2020-09-11 11:20:31 +0200

    k8s

---
+++ ctlserver/index.js
@@ -0,0 +1,25 @@
+
+// Express App Setup
+const port = 3010
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/ctl', (req, res) => {
+  console.log("hier bin ich")
+  res.send('Hi clt server');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hi clt root server');
+});
+
+
+app.listen(port, err => {
+  console.log('Listening port ' + port);
+});
